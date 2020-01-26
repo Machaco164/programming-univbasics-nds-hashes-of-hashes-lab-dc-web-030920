5bdf4f6af3637e6ce9ef214f3a21c24374a19f9c
Raul A Rozo <raul.a.rozo.o@gmail.com>
2020-01-26 23:02:43 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -49,6 +49,12 @@ new_hash = {
        label: "Class",
         sub_category: {
           label: "Order",
+            sub_category: {
+   label: "Family",
+   sub_category: {
+     label: "Genus",
+   }
+ }
         }
      }
    }
